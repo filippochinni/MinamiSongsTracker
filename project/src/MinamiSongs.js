1d0e9db283da7b2f4c00f99105ab7cfd2484601c
filippochinni <filippochinni02@gmail.com>
2023-10-14 22:12:38 +0200

    Initial Commit

---
+++ project/src/MinamiSongs.js
@@ -0,0 +1,472 @@
+/* DEFINIZIONE DI ELEMENTI RUNTIME */
+
+import { MinamiSongsJSON } from "./songsJSON.js";
+
+const localStorageKey = "User";
+
+const MinamiSongs = (localStorage.length > 0) ? initPage() : MinamiSongsJSON;
+
+const subtitleText = `The are currently ("Albumless" included) ${MinamiSongs.length} Minami's Songs`;
+const feedbackText = `The Songs which satisfy the Search Criteria are in total `;
+
+const searchBarPlaceholder = `Search by Name, Album or Year...`;
+
+const trackUpdateButtonText = `Change Status`;
+
+const arrowDownImgPath = `./res/images/downArrow.svg`;
+const arrowUpImgPath = `./res/images/upArrow.svg`;
+const searchButtonPath = `./res/images/search-Button.png`;
+const youtubeIconPath = `./res/images/youtubeIcon.svg`;
+
+const switchStatusSearch = initSwitchStatusSearch();
+const switchStatusTrack = initSwitchStatusTrack();
+const switchStatusSort = initSwitchStatusSort();
+
+let reverseSortStatus = false;
+
+
+
+/* DEFINIZIONE DI ELEMENTI HTML */
+
+const title = document.querySelector("#title");
+const subtitle = document.querySelector("#subtitle");
+const feedback = document.querySelector("#feedback");
+
+const searchBar = document.querySelector("#searchBar");
+
+const searchButtons = document.querySelectorAll(".searchButton");
+const trackButtons = document.querySelectorAll(".trackButton");
+const sortButtons = document.querySelectorAll(".sortButton");
+
+const outputDiv = document.querySelector("#outputDiv");
+
+
+/* FUNZIONI */
+
+function initPage() {
+    let storageObject;
+    
+    if(localStorage.getItem(localStorageKey)) {
+        storageObject = JSON.parse(localStorage.getItem(localStorageKey));
+        return (storageObject.length != MinamiSongsJSON.length) ? updateSongList(storageObject) : storageObject;
+    }
+    else {
+        alert("If you had a previous Session, the data is now lost");
+        return null;
+    }
+}
+
+function updateSongList(storageSongList) {
+    const updatedSongList = MinamiSongsJSON.map((elem) => {
+        let temp = storageSongList.find(song => song["Title"] === elem["Title"]);
+        elem["Check"] = temp ? temp["Check"] : elem["Check"];
+        return elem;
+    });
+
+    return updatedSongList;
+}
+
+function initSwitchStatusSearch() {
+    let switchStatusSearch = {};
+    let propertiesStrings = [];
+    const HTMLElementes = document.querySelectorAll(".searchButton");
+
+    for(let elem of HTMLElementes) {
+        propertiesStrings.push(elem.textContent);
+    }
+
+    for(let i=0; i < propertiesStrings.length; i++) {
+        switchStatusSearch[propertiesStrings[i]] = false;
+    }
+
+    switchStatusSearch[propertiesStrings[0]] = true;
+    HTMLElementes.item(0).classList.toggle("activeButton", true);
+
+    return switchStatusSearch;
+}
+
+function initSwitchStatusTrack() {
+    let switchStatusTrack = {};
+    let propertiesStrings = [];
+    const HTMLElementes = document.querySelectorAll(".trackButton");
+
+    for(let elem of HTMLElementes) {
+        propertiesStrings.push(elem.textContent);
+    }
+
+    for(let i=0; i < propertiesStrings.length; i++) {
+        switchStatusTrack[propertiesStrings[i]] = false;
+    }
+
+    return switchStatusTrack;
+}
+
+function initSwitchStatusSort() {
+    let switchStatusSort = {};
+    let propertiesStrings = [];
+    const HTMLElementes = document.querySelectorAll(".sortButton");
+
+    for(let elem of HTMLElementes) {
+        propertiesStrings.push(elem.textContent.trim());
+    }
+
+    for(let i=0; i < propertiesStrings.length; i++) {
+        switchStatusSort[propertiesStrings[i]] = 0;
+    }
+
+    return switchStatusSort;
+}
+
+function activateOption(option, switchStatus, buttonsArray) {
+    switchStatus[option] = !switchStatus[option];
+
+    for(const key in switchStatus) {
+        if(switchStatus[key] && key != option) {
+            switchStatus[key] = false;
+            buttonsArray.find(elem => elem.textContent == key).classList.toggle("activeButton", false);
+        }    
+    }
+
+    const button = buttonsArray.find(elem => elem.textContent == option);
+    button.classList.toggle("activeButton", switchStatus[option]);
+
+    funzioneMaster();
+}
+
+function activateSortOption(option, switchStatus=switchStatusSort, buttonsArray=[...sortButtons]) {
+    switchStatus[option] === 2 ? switchStatus[option] = 0 : switchStatus[option]++;
+
+    for(const key in switchStatus) {
+        if(switchStatus[key] !== 0 && key != option) {
+            switchStatus[key] = 0;
+            let tempButton = buttonsArray.find(elem => elem.textContent.trim() == key);
+            tempButton.classList.toggle("activeButton", false);
+            tempButton.querySelector("img").src = "";
+            tempButton.querySelector("img").style.display = "None";
+        }    
+    }
+
+    const button = buttonsArray.find(elem => elem.textContent.trim() == option);
+
+    switch(switchStatus[option]) {
+        case 0:
+            button.classList.toggle("activeButton", false);
+            reverseSortStatus = false;
+            button.querySelector("img").src = "";
+            button.querySelector("img").style.display = "None";
+            break;
+        case 1:
+            button.classList.toggle("activeButton", true);
+            reverseSortStatus = false;
+            button.querySelector("img").src = arrowDownImgPath;
+            button.querySelector("img").style.display = "block";
+            break;
+        case 2:
+            //button.classList.toggle("activeButton", true);
+            reverseSortStatus = true;
+            button.querySelector("img").src = arrowUpImgPath;
+            button.querySelector("img").style.display = "block";
+            break;
+        default:
+            console.error("Errore Impossibile");
+    }
+
+    funzioneMaster();
+}
+
+function searchSongs(songsArray=MinamiSongs, reverse=false, inputString) {
+    let resArr = [];
+    const searchCriteria = getSearchCriteria();
+
+    resArr = songsArray.filter(elem => elem[searchCriteria] ? elem[searchCriteria].toLowerCase().includes(inputString.toLowerCase()) : null);
+
+    resArr = applyStatusFilter(resArr, getTrackStatusFilter());
+
+    resArr = applySortCriteria(resArr, getSortCriteria(), reverse);
+
+    feedback.innerHTML = feedbackText + `<span>${resArr.length}</span>`;
+
+    return resArr;
+}
+
+function getSearchCriteria() {
+    let criteria;
+
+    for(const key in switchStatusSearch) {
+        if(switchStatusSearch[key]) {
+            criteria = key;
+        }
+    }
+    
+    switch(criteria) {
+        case "Search by Name":
+            return "Title";
+        case "Search by Album/Single":
+            return "Single/Album";
+        case "Search by Year":
+            return "Year";
+        default:
+            return "Title";
+    }
+}
+
+function applyStatusFilter(songsArray=MinamiSongs, trackStatusCriteria) {
+    if(trackStatusCriteria) {
+        songsArray = songsArray.filter(elem => getTrackStatus(elem) === trackStatusCriteria);
+        
+        feedback.innerHTML = feedbackText + `<span>${songsArray.length}</span>`;
+    }
+    
+    return songsArray;
+}
+
+function getTrackStatusFilter() {
+    let criteria = null;
+
+    for(const key in switchStatusTrack) {
+        if(switchStatusTrack[key]) {
+            criteria = key;
+        }
+    }
+    
+    switch(criteria) {
+        case "Unheard Songs":
+            return "Unheard";
+        case "Heard Songs":
+            return "Heard";
+        case "Playlist Songs":
+            return "Playlist";
+        default:
+            return criteria;
+    }
+}
+
+function getSortCriteria() {
+    let criteria;
+
+    for(const key in switchStatusSort) {
+        if(switchStatusSort[key]) {
+            criteria = key;
+        }
+    }
+    
+    switch(criteria) {
+        case "Song":
+            return "Title";
+        case "Album":
+            return "Single/Album";
+        case "Year":
+            return "Year";
+        default:
+            return "Title";
+    }
+}
+
+function applySortCriteria(songsArray, sortCriteria, reverse) {
+    songsArray.sort((a, b) => a[sortCriteria].toLowerCase() > b[sortCriteria].toLowerCase() ? 1 : -1);
+
+    if(reverse)     songsArray.reverse();
+
+    return songsArray;
+}
+
+function fillOutputDiv(songsArray=MinamiSongs) {
+    outputDiv.innerHTML = "";
+
+    for(const song of songsArray) {
+        const songDiv = createSongBox(song);
+        outputDiv.appendChild(songDiv);
+    }
+}
+
+function createSongBox(song) {
+    const songDiv = document.createElement("div");
+    songDiv.classList.add("songDiv");
+    assignSongDivClass(songDiv, song["Check"]);
+
+    const songNameP = createSongNameP(song["Title"]);
+    const songInfoDiv = createSongInfoDiv(song);
+    const trackUpdateButtonP = createTrackUpdateButtonP(song);
+
+    songDiv.appendChild(songNameP);
+    songDiv.appendChild(songInfoDiv);
+    songDiv.appendChild(trackUpdateButtonP);
+
+    return songDiv;
+}
+
+function assignSongDivClass(songDiv, trackStatus) {
+    switch(trackStatus) {
+        case 0:
+            songDiv.classList.add("statusUnheard");
+            break;
+        case 1:
+            songDiv.classList.add("statusHeard");
+            break;
+        case 2:
+            songDiv.classList.add("statusPlaylist");
+            break;
+        default:
+            return "Errore Impossibile";
+    }
+}
+
+function createSongNameP(songName) {
+    const songNameP = document.createElement("p");
+    songNameP.classList.add("songNameP");
+
+    songNameP.textContent = songName;
+    
+    return songNameP;
+}
+
+function createSongInfoDiv(song) {
+    const songInfoDiv = document.createElement("div");
+    songInfoDiv.classList.add("songInfoDiv");
+    
+    const kanjiTitleP = document.createElement("p");
+    kanjiTitleP.classList.add("songInfoElement");
+    kanjiTitleP.innerHTML = `Kanji Title: <span>${song["Kanji Title"]}</span>`;
+
+    const albumP = document.createElement("p");
+    albumP.classList.add("songInfoElement");
+    albumP.innerHTML = `Album/Single: <span>${song["Single/Album"]}</span>`;
+
+
+    const songLinkP = document.createElement("p");
+    songLinkP.classList.add("songInfoElement");
+    songLinkP.textContent = `Song Youtube Link: `;
+
+    const tempLinkElement1 = document.createElement("a");
+    tempLinkElement1.href = song["Song"];
+    tempLinkElement1.target = "_blank";
+    tempLinkElement1.rel = "noopener noreferrer";   //idk
+    //tempLinkElement1.textContent = "Song Youtube Link";
+    tempLinkElement1.innerHTML = `<span><img src=${youtubeIconPath} class="yt"></span>`;
+    songLinkP.appendChild(tempLinkElement1);
+
+
+    const SearchYTP = document.createElement("p");
+    SearchYTP.classList.add("songInfoElement");
+    SearchYTP.textContent = `Youtube Search: `;
+
+    const tempLinkElement2 = document.createElement("a");
+    tempLinkElement2.href = song["Search on YT"];
+    tempLinkElement2.target = "_blank";
+    tempLinkElement2.rel = "noopener noreferrer";   //idk
+    //tempLinkElement2.textContent = "Song Search Link";
+    tempLinkElement2.innerHTML = `<img src=${searchButtonPath}>`;
+    SearchYTP.appendChild(tempLinkElement2);
+
+
+    const yearP = document.createElement("p");
+    yearP.classList.add("songInfoElement");
+    yearP.innerHTML = `Year: <span>${song["Year"]}</span>`;
+
+    const trackStatusP = document.createElement("p");
+    trackStatusP.classList.add("songInfoElement");
+    trackStatusP.innerHTML = `Status: <span class="Status">${getTrackStatus(song)}</span>`;
+
+    songInfoDiv.appendChild(kanjiTitleP);
+    songInfoDiv.appendChild(albumP);
+    songInfoDiv.appendChild(songLinkP);
+    songInfoDiv.appendChild(SearchYTP);
+    songInfoDiv.appendChild(yearP);
+    songInfoDiv.appendChild(trackStatusP);
+
+    return songInfoDiv;
+}
+
+function createTrackUpdateButtonP(song) {
+    const trackUpdateButtonP = document.createElement("p");
+    trackUpdateButtonP.classList.add("trackUpdateButtonP");
+
+    const trackUpdateButton = document.createElement("button");
+    trackUpdateButton.classList.add("trackUpdateButton");
+    trackUpdateButton.textContent = trackUpdateButtonText;
+
+    trackUpdateButton.addEventListener("click", () => { updateTrackStatus(MinamiSongs, song, trackUpdateButton.parentElement) });
+
+    trackUpdateButtonP.appendChild(trackUpdateButton);
+
+    return trackUpdateButtonP;
+}
+
+function getTrackStatus(song) {
+    const trackStatus = song["Check"];
+
+    switch(trackStatus) {
+        case 0:
+            return "Unheard";
+        case 1:
+            return "Heard";
+        case 2:
+            return "Playlist";
+        default:
+            return "Errore Impossibile";
+    }
+}
+
+function updateTrackStatus(songList=MinamiSongs, song, songDiv) {
+    const trackStatus = song["Check"];
+    const songIndex = songList.findIndex(elem => elem["Title"] === song["Title"]);
+
+    switch(trackStatus) {
+        case 0:
+            songList[songIndex]["Check"]++;
+            break;
+        case 1:
+            songList[songIndex]["Check"]++;
+            break;
+        case 2:
+            songList[songIndex]["Check"] = 0;
+            break;
+        default:
+            return "Errore Impossibile";
+    }
+
+    saveSession(songList);
+    funzioneMaster();
+}
+
+function saveSession(songList=MinamiSongs) {
+    const sessionObject = JSON.stringify(songList);
+    localStorage.setItem(localStorageKey, sessionObject);
+}
+
+function funzioneMaster() {
+    const inputString = document.querySelector("#searchBar").value;
+
+    if(!inputString) {
+        feedback.innerHTML = "";
+        fillOutputDiv(applySortCriteria(applyStatusFilter(MinamiSongs, getTrackStatusFilter()), getSortCriteria(), reverseSortStatus));
+        return;
+    }
+
+    const resSongsArr = searchSongs(MinamiSongs, reverseSortStatus, inputString);
+
+    fillOutputDiv(resSongsArr);
+}
+
+
+/* ESECUZIONE */
+
+subtitle.textContent = subtitleText;
+
+searchBar.placeholder = searchBarPlaceholder;
+
+for(const elem of searchButtons) {
+    elem.addEventListener("click", () => { activateOption(elem.textContent, switchStatusSearch, [...searchButtons]) });
+}
+for(const elem of trackButtons) {
+    elem.addEventListener("click", () => { activateOption(elem.textContent, switchStatusTrack, [...trackButtons]) });
+}
+for(const elem of sortButtons) {
+    elem.addEventListener("click", () => { activateSortOption(elem.textContent.trim(), switchStatusSort, [...sortButtons]) });
+}
+
+searchBar.addEventListener("input", funzioneMaster);
+
+funzioneMaster();
+
+
---
+++ project/src/songsJSON.js
@@ -0,0 +1,362 @@
+export const MinamiSongsJSON = [
+    {
+        "Title": "Ame wo Matsu",
+        "Kanji Title": "アメヲマツ、",
+        "Song": "https://www.youtube.com/watch?v=766qmHTc2ro",
+        "Search on YT": "https://m.youtube.com/results?sp=mAEA&search_query=Ame wo Matsu+Minami",
+        "Single/Album": "Amewomatsu | DROP",
+        "Year": "2020 | 2021",
+        "Check": 0
+    },
+    {
+        "Title": "BLANK POST",
+        "Kanji Title": "//",
+        "Song": "https://www.youtube.com/watch?v=YMMLSPveevI&ab_channel=Minami-Topic",
+        "Search on YT": "https://m.youtube.com/results?sp=mAEA&search_query=BLANK POST+Minami",
+        "Single/Album": "LOSE LOOSE DAY",
+        "Year": "2023",
+        "Check": 0
+    },
+    {
+        "Title": "BLUE GURASU",
+        "Kanji Title": "ブルーグラス",
+        "Song": "https://www.youtube.com/watch?v=nzXmX5XGjnQ",
+        "Search on YT": "https://m.youtube.com/results?sp=mAEA&search_query=BLUE GURASU+Minami",
+        "Single/Album": "LOSE LOOSE DAY",
+        "Year": "2023",
+        "Check": 0
+    },
+    {
+        "Title": "DROP",
+        "Kanji Title": "//",
+        "Song": "https://www.youtube.com/watch?v=WpRcRYoHqxE",
+        "Search on YT": "https://m.youtube.com/results?sp=mAEA&search_query=DROP+Minami",
+        "Single/Album": "DROP",
+        "Year": "2021",
+        "Check": 0
+    },
+    {
+        "Title": "Freiheit",
+        "Kanji Title": "フライハイト",
+        "Song": "https://www.youtube.com/watch?v=UquKfPB_NeE&ab_channel=Minami-Topic",
+        "Search on YT": "https://m.youtube.com/results?sp=mAEA&search_query=Freiheit+Minami",
+        "Single/Album": "DROP",
+        "Year": "2021",
+        "Check": 0
+    },
+    {
+        "Title": "GOOD LUCKER",
+        "Kanji Title": "グッドラッカー",
+        "Song": "https://www.youtube.com/watch?v=ZYajwLQIavE",
+        "Search on YT": "https://m.youtube.com/results?sp=mAEA&search_query=GOOD LUCKER+Minami",
+        "Single/Album": "GOOD LUCKER | LOSE LOOSE DAY",
+        "Year": "2022 | 2023",
+        "Check": 0
+    },
+    {
+        "Title": "Hollowness",
+        "Kanji Title": "ホロネス",
+        "Song": "https://www.youtube.com/watch?v=HIRiduzNLzQ",
+        "Search on YT": "https://m.youtube.com/results?sp=mAEA&search_query=Hollowness+Minami",
+        "Single/Album": "Kawaki Wo Ameku",
+        "Year": "2019",
+        "Check": 0
+    },
+    {
+        "Title": "Kawaki wo Ameku",
+        "Kanji Title": "カワキヲアメク",
+        "Song": "https://www.youtube.com/watch?v=0YF8vecQWYs",
+        "Search on YT": "https://m.youtube.com/results?sp=mAEA&search_query=Kawaki wo Ameku+Minami",
+        "Single/Album": "Kawaki Wo Ameku",
+        "Year": "2019",
+        "Check": 0
+    },
+    {
+        "Title": "Kimi To Boku No 154 Shosetsu Senso",
+        "Kanji Title": "君と僕の１５４小節戦争",
+        "Song": "https://www.youtube.com/watch?v=G0eVUgX4PDo&ab_channel=Minami-Topic",
+        "Search on YT": "https://m.youtube.com/results?sp=mAEA&search_query=Kimi To Boku No 154 Shosetsu Senso+Minami",
+        "Single/Album": "DROP",
+        "Year": "2021",
+        "Check": 0
+    },
+    {
+        "Title": "Kono Machi Ni Hare Wa Konai",
+        "Kanji Title": "この街に晴れはこない",
+        "Song": "https://www.youtube.com/watch?v=kCAR6BGPBxY",
+        "Search on YT": "https://m.youtube.com/results?sp=mAEA&search_query=Kono Machi Ni Hare Wa Konai+Minami",
+        "Single/Album": "DROP",
+        "Year": "2021",
+        "Check": 0
+    },
+    {
+        "Title": "Lilac",
+        "Kanji Title": "ライラック",
+        "Song": "https://www.youtube.com/watch?v=GQ3V50XoLOM",
+        "Search on YT": "https://m.youtube.com/results?sp=mAEA&search_query=Lilac+Minami",
+        "Single/Album": "Kawaki Wo Ameku",
+        "Year": "2019",
+        "Check": 0
+    },
+    {
+        "Title": "Main Actor",
+        "Kanji Title": "//",
+        "Song": "https://www.youtube.com/watch?v=jb4ybTQwcdw",
+        "Search on YT": "https://m.youtube.com/results?sp=mAEA&search_query=Main Actor+Minami",
+        "Single/Album": "main actor | Kawaki Wo Ameku",
+        "Year": "2017 | 2019",
+        "Check": 0
+    },
+    {
+        "Title": "Monologue",
+        "Kanji Title": "//",
+        "Song": "https://www.youtube.com/watch?v=vrKOiiiGidI",
+        "Search on YT": "https://m.youtube.com/results?sp=mAEA&search_query=Monologue+Minami",
+        "Single/Album": "ETERNAL BLUE",
+        "Year": "2018",
+        "Check": 0
+    },
+    {
+        "Title": "Prologue",
+        "Kanji Title": "プロローグ",
+        "Song": "https://www.youtube.com/watch?v=DkAyAvKMFP4&ab_channel=Minami-Topic",
+        "Search on YT": "https://m.youtube.com/results?sp=mAEA&search_query=Prologue+Minami",
+        "Single/Album": "main actor | Kawaki Wo Ameku",
+        "Year": "2017 | 2019",
+        "Check": 0
+    },
+    {
+        "Title": "RUDE LOSE DANCE",
+        "Kanji Title": "ルードルーズダンス",
+        "Song": "https://www.youtube.com/watch?v=F86GZpO_ZeU",
+        "Search on YT": "https://m.youtube.com/results?sp=mAEA&search_query=RUDE LOSE DANCE+Minami",
+        "Single/Album": "RUDE LOSE DANCE | LOSE LOOSE DAY",
+        "Year": "2023 | 2023",
+        "Check": 0
+    },
+    {
+        "Title": "Sensei, Ano ne",
+        "Kanji Title": "先生、あのね",
+        "Song": "https://www.youtube.com/watch?v=-rHtKuhv8bU",
+        "Search on YT": "https://m.youtube.com/results?sp=mAEA&search_query=Sensei, Ano ne+Minami",
+        "Single/Album": "Emotional Water",
+        "Year": "2017",
+        "Check": 0
+    },
+    {
+        "Title": "Shoujiki Nikki",
+        "Kanji Title": "正直日記",
+        "Song": "https://www.youtube.com/watch?v=DOES+NOT+EXISTS",
+        "Search on YT": "https://m.youtube.com/results?sp=mAEA&search_query=Shoujiki Nikki+Minami",
+        "Single/Album": "Emotional Water",
+        "Year": "2017",
+        "Check": 0
+    },
+    {
+        "Title": "Shoujiki Nikki long ver.",
+        "Kanji Title": "正直日記 long ver.",
+        "Song": "https://www.youtube.com/watch?v=qHtz9s3UXsM",
+        "Search on YT": "https://m.youtube.com/results?sp=mAEA&search_query=Shoujiki Nikki long ver.+Minami",
+        "Single/Album": "ETERNAL BLUE",
+        "Year": "2018",
+        "Check": 0
+    },
+    {
+        "Title": "Suichuu Reflection",
+        "Kanji Title": "水中リフレクション",
+        "Song": "https://www.youtube.com/watch?v=TB2Kj9coITw",
+        "Search on YT": "https://m.youtube.com/results?sp=mAEA&search_query=Suichuu Reflection+Minami",
+        "Single/Album": "Emotional Water",
+        "Year": "2017",
+        "Check": 0
+    },
+    {
+        "Title": "TIME GRAM",
+        "Kanji Title": "タイムグラム",
+        "Song": "https://www.youtube.com/watch?v=-yW8oVFQ7TE",
+        "Search on YT": "https://m.youtube.com/results?sp=mAEA&search_query=TIME GRAM+Minami",
+        "Single/Album": "LOSE LOOSE DAY",
+        "Year": "2023",
+        "Check": 0
+    },
+    {
+        "Title": "Umi to Natsu to Kimi to",
+        "Kanji Title": "海と夏と君と",
+        "Song": "https://www.youtube.com/watch?v=k2tQhCGqw8g",
+        "Search on YT": "https://m.youtube.com/results?sp=mAEA&search_query=Umi to Natsu to Kimi to+Minami",
+        "Single/Album": "Emotional Water",
+        "Year": "2017",
+        "Check": 0
+    },
+    {
+        "Title": "Astor",
+        "Kanji Title": "アスター",
+        "Song": "https://soundcloud.com/pineapple_cookie/minami-astor-from-twitcasting",
+        "Search on YT": "https://www.google.com/search?q=Astor+Minami",
+        "Single/Album": " ",
+        "Year": " ",
+        "Check": 0
+    },
+    {
+        "Title": "Haru",
+        "Kanji Title": "ハル -あと3cmの冬-",
+        "Song": "https://minami-collections.netlify.app/player.html?s=discography",
+        "Search on YT": "https://www.google.com/search?q=Haru+Minami",
+        "Single/Album": "Intent",
+        "Year": "2015",
+        "Check": 0
+    },
+    {
+        "Title": "F and B",
+        "Kanji Title": "FとB",
+        "Song": "https://soundcloud.com/free-freth-almazan/fb-minami",
+        "Search on YT": "https://www.google.com/search?q=F and B+Minami",
+        "Single/Album": "Intent",
+        "Year": "2015",
+        "Check": 0
+    },
+    {
+        "Title": "groping",
+        "Kanji Title": "//",
+        "Song": "https://www.youtube.com/watch?v=DOES+NOT+EXISTS",
+        "Search on YT": "https://www.google.com/search?q=groping+Minami",
+        "Single/Album": " ",
+        "Year": " ",
+        "Check": 0
+    },
+    {
+        "Title": "issue",
+        "Kanji Title": "//",
+        "Song": "https://www.youtube.com/watch?v=QkPuGuH7fQg&ab_channel=Merazir",
+        "Search on YT": "https://m.youtube.com/results?sp=mAEA&search_query=issue+Minami",
+        "Single/Album": " ",
+        "Year": " ",
+        "Check": 0
+    },
+    {
+        "Title": "Pressure Boys",
+        "Kanji Title": "プレッシャーボーイズ",
+        "Song": "https://www.youtube.com/watch?v=DOES+NOT+EXISTS",
+        "Search on YT": "https://www.google.com/search?q=プレッシャーボーイズ+Minami",
+        "Single/Album": " ",
+        "Year": " ",
+        "Check": 0
+    },
+    {
+        "Title": "Deep-sea fish",
+        "Kanji Title": "//",
+        "Song": "https://www.youtube.com/watch?v=DOES+NOT+EXISTS",
+        "Search on YT": "https://www.google.com/search?q=Deep-sea fish+Minami",
+        "Single/Album": " ",
+        "Year": " ",
+        "Check": 0
+    },
+    {
+        "Title": "After School Orange Hero",
+        "Kanji Title": "放課後オレンジヒーロー",
+        "Song": "https://www.youtube.com/watch?v=DOES+NOT+EXISTS",
+        "Search on YT": "https://www.google.com/search?q=放課後オレンジヒーロー+Minami",
+        "Single/Album": " ",
+        "Year": " ",
+        "Check": 0
+    },
+    {
+        "Title": "ETERNAL BLUE",
+        "Kanji Title": "//",
+        "Song": "https://www.youtube.com/watch?v=YS1mNPAxnLE",
+        "Search on YT": "https://m.youtube.com/results?sp=mAEA&search_query=ETERNAL BLUE+Minami",
+        "Single/Album": " ",
+        "Year": " ",
+        "Check": 0
+    },
+    {
+        "Title": "{ ??? }",
+        "Kanji Title": "留年確定",
+        "Song": "https://www.youtube.com/watch?v=DOES+NOT+EXISTS",
+        "Search on YT": "https://www.google.com/search?q=留年確定+Minami",
+        "Single/Album": " ",
+        "Year": " ",
+        "Check": 0
+    },
+    {
+        "Title": "Curry Rice",
+        "Kanji Title": "カレーライス",
+        "Song": "https://www.youtube.com/watch?v=DOES+NOT+EXISTS",
+        "Search on YT": "https://www.google.com/search?q=カレーライス+Minami",
+        "Single/Album": " ",
+        "Year": " ",
+        "Check": 0
+    },
+    {
+        "Title": "Like a Cider",
+        "Kanji Title": "サイダーみたいだ",
+        "Song": "https://www.youtube.com/watch?v=2k3PchLHThQ",
+        "Search on YT": "https://m.youtube.com/results?sp=mAEA&search_query=サイダーみたいだ+Minami",
+        "Single/Album": " ",
+        "Year": " ",
+        "Check": 0
+    },
+    {
+        "Title": "{ ??? }",
+        "Kanji Title": "傘を忘れた日",
+        "Song": "https://www.youtube.com/watch?v=DOES+NOT+EXISTS",
+        "Search on YT": "https://www.google.com/search?q=傘を忘れた日+Minami",
+        "Single/Album": " ",
+        "Year": " ",
+        "Check": 0
+    },
+    {
+        "Title": "Summertime game!!",
+        "Kanji Title": "//",
+        "Song": "https://www.youtube.com/watch?v=DOES+NOT+EXISTS",
+        "Search on YT": "https://www.google.com/search?q=Summertime game!!+Minami",
+        "Single/Album": " ",
+        "Year": " ",
+        "Check": 0
+    },
+    {
+        "Title": "{ ??? }",
+        "Kanji Title": "星屑のうた",
+        "Song": "https://www.youtube.com/watch?v=DOES+NOT+EXISTS",
+        "Search on YT": "https://www.google.com/search?q=星屑のうた+Minami",
+        "Single/Album": " ",
+        "Year": " ",
+        "Check": 0
+    },
+    {
+        "Title": "Yukimi Daifuku no Uta",
+        "Kanji Title": "雪見だいふくの歌",
+        "Song": "https://www.youtube.com/watch?v=zHn3aMtkQDA",
+        "Search on YT": "https://m.youtube.com/results?sp=mAEA&search_query=雪見だいふくの歌+Minami",
+        "Single/Album": " ",
+        "Year": " ",
+        "Check": 0
+    },
+    {
+        "Title": "Very Very Strawberry",
+        "Kanji Title": "//",
+        "Song": "https://www.youtube.com/watch?v=QOywg1VEQ1s",
+        "Search on YT": "https://m.youtube.com/results?sp=mAEA&search_query=Very Very Strawberry+Minami",
+        "Single/Album": "Intent",
+        "Year": "2015",
+        "Check": 0
+    },
+    {
+        "Title": "Henna",
+        "Kanji Title": "ヘナ",
+        "Song": "https://www.youtube.com/watch?v=bZcgBfI813k",
+        "Search on YT": "https://m.youtube.com/results?sp=mAEA&search_query=Henna+Minami",
+        "Single/Album": " ",
+        "Year": " ",
+        "Check": 0
+    },
+    {
+        "Title": "Professional Girl",
+        "Kanji Title": "//",
+        "Song": "https://www.youtube.com/watch?v=BdRnqASK0IA",
+        "Search on YT": "https://m.youtube.com/results?sp=mAEA&search_query=Professional Girl+Minami",
+        "Single/Album": " ",
+        "Year": " ",
+        "Check": 0
+    }
+];
